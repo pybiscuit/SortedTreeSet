a42c6d98daff9724a9dd690b4ee9b64b46ec96f3
pybiscuit <chemistryrob@gmail.com>
2025-06-21 21:16:59 -0600

    Implemented SortedTreeSet interface, added comments, build out main and updated.

--- Main.java
+++ Main.java
@@ -1,5 +1,74 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
 class Main {
     public static void main(String[] args) {
-        System.out.print("Start");
-    }
+       // open the file passed via command line args.
+		File inputFile = new File(args[0]);
+		char skipFirstRow = 'Y';
+        char firstPerson = 'Y';
+		
+		// PersonImperialSet listOfPeople = new PersonImperialSet();
+		SortedTreeSet alphaList = new SortedTreeSet();
+
+		String path = new File("").getAbsolutePath();
+
+		// test person - not used in final version, requested in pt. 1
+		// Person newPerson = new Person("Marcus", 72, 111);
+
+		try {
+			// open a scanner to read the file. try-catch for ioexceptions.
+			Scanner scnr = new Scanner(inputFile);
+
+			// loop over scanner nextlines (skipping the first for column headers)
+			// extracting name, height, wegiht.
+			// add the person data to the list of people.
+			while (scnr.hasNextLine()) {
+
+				if (skipFirstRow == 'Y') {
+                    skipFirstRow = 'N';
+					scnr.nextLine();
+                    continue;
+                }
+
+				String name = scnr.next();
+				double height = scnr.nextDouble();
+				double weight = scnr.nextDouble();
+				
+				// Person tmpPerson = new Person(name, height, weight);
+				Person tmpPerson1 = new Person(name, height, weight);
+				// listOfPeople.add(tmpPerson);
+                if (firstPerson == 'Y') {
+                    alphaList = new SortedTreeSet(tmpPerson1);
+                    firstPerson = 'N';
+                } else {
+                    alphaList.add(tmpPerson1);
+                }
+				
+			}
+
+			scnr.close();
+
+		} catch (IOException e) {
+			System.out.print("File not found.");
+			e.printStackTrace();
+		}
+		// alphaList.sort();
+		System.out.print(alphaList + "\n");
+		// System.out.print(listOfPeople);		
+
+		// try {
+		// 	FileWriter fWriter = new FileWriter(path + "\\hr_imperial_set_output.txt");
+		// 	fWriter.write(listOfPeople.toString());
+		// 	fWriter.close();
+
+		// 	FileWriter fWriter2 = new FileWriter(path + "\\hr_ordered_set_output.txt");
+		// 	fWriter2.write(alphaList.toString());
+		// 	fWriter2.close();
+
+		// } catch (IOException e) {
+		// 	System.out.print(e.getMessage());
+		// }
+	}
 }
--- SortedTreeSet.java
+++ SortedTreeSet.java
@@ -1,14 +1,105 @@
 class SortedTreeSet implements SortedTreeSetInterface{
-    
-    public Person getPerson();
+    private Person data;
+    private SortedTreeSet left;
+    private SortedTreeSet right;
+	private String stringOuput = "Name    Height (cm)    Weight (kg)";
 
-	public boolean hasLeft();
-	public void setLeft(SortedTreeSet left);
-	public SortedTreeSet getLeft();
+	// empty constructor used for intialization.
+	public SortedTreeSet() {
 
-	public boolean hasRight();
-	public void setRight(SortedTreeSet right);
-	public SortedTreeSet getRight();
+    }
 
-	public void add(Person p);
+	// constructor that sets the root node.
+    public SortedTreeSet(Person data) {
+        this.data = data;
+    }
+
+	// returns data jor the current node.
+    @Override
+    public Person getPerson() {
+        return this.data;
+    }
+
+	// checks if the current node has a left reference for a node.
+    @Override
+    public boolean hasLeft() {
+        return this.left != null;
+    }
+
+	// sets the left reference for the current node.
+    @Override
+    public void setLeft(SortedTreeSet left) {
+        this.left = left;
+    }
+
+	// returns the left reference for the current node.
+    @Override
+    public SortedTreeSet getLeft() {
+        return this.left;
+    }
+
+	// checks if the current node has a right reference for a node.
+    @Override
+    public boolean hasRight() {
+        return this.right != null;
+    }
+
+	// sets the right reference for the current node.
+    @Override
+    public void setRight(SortedTreeSet right) {
+        this.right = right;
+    }
+
+	// returns the right reference for the current node.
+    @Override
+    public SortedTreeSet getRight() {
+        return this.right;
+    }
+
+	// adds a reference to a new node to the current node.
+	// uses compareToIgnoreCase (returns a positive or negative number based on
+	// the lexicographical value of the two string [alphabatizes insertion])
+	// https://www.geeksforgeeks.org/java/java-string-comparetoignorecase-method/
+    @Override
+    public void add(Person p) {
+        int compareName = p.getName().compareToIgnoreCase(data.getName());
+        if (compareName < 0) {
+            if (this.hasLeft()) {
+                left.add(p);
+            } else {
+                this.setLeft(new SortedTreeSet(p));
+            }
+        } else if (compareName > 0) {
+            if (this.hasRight()) {
+                right.add(p);
+            } else {
+                this.setRight(new SortedTreeSet(p));
+            }
+        }
+    }
+
+	// Traverse the binary tree setting the output string for the toString method.
+	// Implements in order traversal: https://www.geeksforgeeks.org/dsa/binary-tree-traversal/
+	public void treeTraversal(SortedTreeSet root) {
+		if (root == null) return;
+
+		treeTraversal(root.left);
+		
+		int nameSpace = 8 - root.data.getName().length();
+		int spaceCount = (root.data.getHeight() > 100) ? 10: 11;
+
+		this.stringOuput = this.stringOuput + "\n" 
+			+ root.data.getName() + " ".repeat(nameSpace)
+			+ root.data.getHeight() + " ".repeat(spaceCount)
+			+ root.data.getWeight();
+
+		treeTraversal(root.right);
+
+	}
+
+	@Override
+    public String toString() {
+		this.treeTraversal(this);
+		return this.stringOuput;
+    }
 }
